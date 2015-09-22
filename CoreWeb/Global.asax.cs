5b3b722ab5f413cb0c5b50942fc526d7ed17f7dd
Kane Wang <kanewang@live.com>
2015-09-22 11:39:38 -0700

    another structure change

--- CoreWeb/Global.asax.cs
+++ CoreWeb/Global.asax.cs
@@ -1,5 +1,14 @@
-﻿using System.Web.Optimization;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Web.Optimization;
+using Base.ActivityInterface;
+using Base.Dto;
+using CoreImplementation;
+using CoreImplementation.Validator;
+using FluentValidation;
+using Microsoft.Practices.Unity;
 using WebCoreLibrary;
+using WorkflowActivityLibrary;
 
 namespace CoreWeb
 {
@@ -10,6 +19,21 @@ namespace CoreWeb
             base.Application_Start();
 
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Overwrite implementation
+            var container = UnityRegistration.Container;
+            var assemblyList = new List<Assembly> {
+                Assembly.GetAssembly(typeof (ICoreFlowProvider)), 
+                Assembly.GetAssembly(typeof (CoreFlowProvider))
+            };
+
+            container.RegisterTypes(AllClasses.FromAssemblies(assemblyList), WithMappings.FromMatchingInterface,
+                WithName.Default,
+                WithLifetime.ContainerControlled, null, true);  // overwrite core implementation
+
+            // Overwrite validation
+            container.RegisterType<IValidator<EmailDto>, EmailValidator>(new TransientLifetimeManager());
+
         }
     }
 }
